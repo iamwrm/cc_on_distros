a400088622e66abe16cb8383c7f18569b826a991
Ren Wang <iamwrm@gmail.com>
2022-01-04 13:50:39 +0000

    add build script

---
+++ download_and_build.sh
@@ -0,0 +1,17 @@
+
+# https://llvm.org/docs/GettingStarted.html
+
+gcc_version=11.2.0
+
+wget https://ftp.gnu.org/gnu/gcc/gcc-${gcc_version}/gcc-${gcc_version}.tar.gz
+tar xvf gcc-${gcc_version}.tar.gz
+
+cd gcc-${gcc_version}
+./contrib/download_prerequisites
+cd ..
+mkdir gcc-${gcc_version}-build
+cd gcc-${gcc_version}-build
+
+$PWD/../gcc-${gcc_version}/configure --prefix=$HOME/toolchains --enable-languages=c,c++
+make -j$(nproc)
+make install
