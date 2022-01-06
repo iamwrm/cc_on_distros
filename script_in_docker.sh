4dccc4f7916252e293ce5eb1b1f7251a9f86f882
iamwrm <iamwrm@gmail.com>
2022-01-06 13:22:49 +0800

    add configure

--- script_in_docker.sh
+++ script_in_docker.sh
@@ -1,5 +1,15 @@
 ls
 pwd
 uname -a
-GCC_PKG=$(ls gcc*tar.gz)
-echo $GCC_PKG
+gcc_tar=$(ls gcc*tar.gz)
+echo ${gcc_tar}
+gcc_dir=${gcc_tar::-7}
+echo ${gcc_dir}
+ls
+
+cd ${gcc_dir}
+./contrib/download_prerequisites
+cd ..
+mkdir ${gcc_dir}-build
+cd ${gcc_dir}-build
+$PWD/../${gcc_dir}/configure --prefix=$HOME/toolchains --enable-languages=c,c++ --disable-multilib
