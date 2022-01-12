8e27e22f06ef12011675de1275b700d92f9b9f2e
Ren Wang <iamwrm@gmail.com>
2022-01-12 15:48:47 +0000

    add cmake install for centos

--- ci/distros/centos_7/distro.sh
+++ ci/distros/centos_7/distro.sh
@@ -1,5 +1,11 @@
 set -xe
 
-yum install -y wget git curl ca-certificates cmake python3 python3-pip
+yum install -y wget git curl ca-certificates python3 python3-pip
 yum groupinstall -y "Development tools"
 
+wget -c https://github.com/Kitware/CMake/releases/download/v3.22.1/cmake-3.22.1.tar.gz
+tar xvf cmake-3.22.1.tar.gz
+cd cmake-3.22.1
+./configure --prefix=/usr/local
+make -j$(nproc)
+make install
