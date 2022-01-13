6d32a8357486c06376e05e590691a9212ae5a038
Ren Wang <iamwrm@gmail.com>
2022-01-13 01:05:55 +0000

    centos7 openssl-devel

--- ci/distros/centos_7/distro.sh
+++ ci/distros/centos_7/distro.sh
@@ -1,6 +1,6 @@
 set -xe
 
-yum install -y wget git curl ca-certificates python3 python3-pip openssl
+yum install -y wget git curl ca-certificates python3 python3-pip openssl-devel
 yum groupinstall -y "Development tools"
 
 wget -c https://github.com/Kitware/CMake/releases/download/v3.22.1/cmake-3.22.1.tar.gz
