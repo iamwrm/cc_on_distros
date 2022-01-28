55f527b22cbb22c78634c3a156a69636593fca65
Ren Wang <iamwrm@gmail.com>
2022-01-29 00:04:42 +0800

    try cd to a tmp dir

--- ci/compilers/clang_trunk/compiler.sh
+++ ci/compilers/clang_trunk/compiler.sh
@@ -1,5 +1,8 @@
 set -xe
 
+mkdir -p /tmp/ci_build
+cd /tmp/ci_build
+
 echo "Setup mold for faster building"
 wget -c https://nightly.link/ren15/mold_ci/workflows/build/main/mold_complie_art.zip 
 pwd
@@ -12,6 +15,7 @@ mkdir -p /usr/local/lib/mold
 mv mold /usr/local/bin
 mv mold-wrapper.so /usr/local/lib/mold
 
+echo "Cloning and building llvm"
 git clone --depth 1 https://github.com/llvm/llvm-project.git
 
 cd llvm-project
