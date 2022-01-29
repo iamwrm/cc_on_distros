39a0d8c5b73a4d31221031996d3e1ea0a644d349
Ren Wang <iamwrm@gmail.com>
2022-01-29 16:12:42 +0000

    add ninja build and cmake3

--- ci/distros/centos_7/distro.sh
+++ ci/distros/centos_7/distro.sh
@@ -1,14 +1,16 @@
 set -xe
 
-yum install -y wget git curl ca-certificates \
+yum -y install \
+    epel-release \
+    dnf \
+    ca-certificates
+
+dnf install -y wget git curl \
     python3 python3-pip openssl-devel \
     unzip \
+    cmake3 \
     ninja-build
+
 yum groupinstall -y "Development tools"
 
-wget -c https://github.com/Kitware/CMake/releases/download/v3.22.1/cmake-3.22.1.tar.gz
-tar xvf cmake-3.22.1.tar.gz
-cd cmake-3.22.1
-./configure --prefix=/usr/local
-make -j$(nproc)
-make install
+ln -sf /usr/bin/cmake3 /usr/bin/cmake
