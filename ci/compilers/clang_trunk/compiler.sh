619b81371d380085f17f86cc317b53644c56d99d
Ren Wang <iamwrm@gmail.com>
2022-01-29 17:07:35 +0000

    try install gcc-10 on centos 7

--- ci/compilers/clang_trunk/compiler.sh
+++ ci/compilers/clang_trunk/compiler.sh
@@ -1,19 +1,14 @@
 set -xe
 
+echo "Check gcc version"
+gcc --version
+which gcc
+
 mkdir -p /tmp/ci_build
 cd /tmp/ci_build
 
 echo "Setup mold for faster building"
-wget -c https://nightly.link/ren15/mold_ci/workflows/build/main/mold_complie_art.zip 
-pwd
-ls
-ls ./*.zip | xargs unzip
-
-chmod +x mold
-mkdir -p /usr/local/lib/mold
-
-mv mold /usr/local/bin
-mv mold-wrapper.so /usr/local/lib/mold
+curl -fsSL https://raw.githubusercontent.com/ren15/mold_ci/HEAD/install.sh | bash
 
 echo "Cloning and building llvm"
 git clone --depth 1 https://github.com/llvm/llvm-project.git
--- ci/distros/centos_7/distro.sh
+++ ci/distros/centos_7/distro.sh
@@ -9,8 +9,17 @@ dnf install -y wget git curl \
     python3 python3-pip openssl-devel \
     unzip \
     cmake3 \
+    which \
     ninja-build
 
 yum groupinstall -y "Development tools"
 
 ln -sf /usr/bin/cmake3 /usr/bin/cmake
+
+## get gcc-10
+
+yum install centos-release-scl
+yum install devtoolset-10-gcc
+scl enable devtoolset-10 bash
+which gcc
+gcc --version
