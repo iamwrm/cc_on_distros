cfa7493a8c6bbe7f64c5dc272c8c0c0d4347070b
Ren Wang <iamwrm@gmail.com>
2022-01-29 17:10:19 +0000

    add debian

---
+++ ci/distros/debian_11/distro.sh
@@ -0,0 +1,13 @@
+set -xe
+
+export DEBIAN_FRONTEND="noninteractive"
+
+apt-get update
+apt-get install -y build-essential \
+    wget \
+    curl \
+    cmake \
+    python3 python3-pip \
+    ninja-build \
+    unzip \
+    git
