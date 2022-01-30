0b02eecb49af050246c6606cb9eb9f4fe4443e8a
Ren Wang <iamwrm@gmail.com>
2022-01-30 05:12:00 +0000

    add g++ installation

--- ci/distros/centos_7/distro.sh
+++ ci/distros/centos_7/distro.sh
@@ -19,8 +19,10 @@ ln -sf /usr/bin/cmake3 /usr/bin/cmake
 ## get gcc-10
 
 yum install -y centos-release-scl
-yum install -y devtoolset-10-gcc
+yum install -y devtoolset-10*
 scl enable devtoolset-10 'which gcc'
 scl enable devtoolset-10 'gcc --version'
+scl enable devtoolset-10 'which g++'
+scl enable devtoolset-10 'g++ --version'
 
 echo "source /opt/rh/devtoolset-10/enable \n" > /tmp/setup_cc.sh
